e6fb75739abb1320ecb5ce77c7362129123de4e4
hmorfin <hectormorfin11@gmail.com>
2023-12-07 23:37:40 -0700

    Gnome Sort implemented

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -6,6 +6,19 @@ public class SortingUtility {
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
         // TODO implement Gnome Sort here
+        int pos = 0;
+        while (pos < data.length) {
+            if (pos == 0 || data[pos].compareTo(data[pos - 1]) >= 0) {
+                pos++;
+            } else {
+                // Swap data[pos] and data[pos-1]
+                T temp = data[pos];
+                data[pos] = data[pos - 1];
+                data[pos - 1] = temp;
+
+                pos--;
+            }
+        }
     }
 
 
